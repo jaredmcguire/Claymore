7ceb7ed44ff7a1c90e915b2c1bed8db87d6e1da4
Jared McGuire <jared.mcguire@gmail.com>
2011-08-31 09:26:43 -0500

    update assembly info

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -1,2 +1,1 @@
 ilmerge /target:exe /out:Claymore.exe ..\Claymore\bin\Release\Claymore.exe ..\Claymore\bin\Release\Microsoft.Practices.ServiceLocation.dll ..\Claymore\bin\Release\Microsoft.Practices.Unity.dll ..\Claymore\bin\Release\CommandLine.dll ..\Claymore\bin\Release\ArcLight.Core.dll ..\Claymore\bin\Release\ArcLight.Unity.dll /v4
-pause
--- Claymore/Properties/AssemblyInfo.cs
+++ Claymore/Properties/AssemblyInfo.cs
@@ -2,10 +2,11 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Claymore")]
-[assembly: AssemblyCompany("Jared McGuire")]
 [assembly: AssemblyProduct("Claymore")]
-[assembly: AssemblyCopyright("Copyright Jared McGuire")]
+[assembly: AssemblyCopyright("Jared McGuire <jared.mcguire@gmail.com>")]
+[assembly: AssemblyDescription("Claymore | Jared McGuire <jared.mcguire@gmail.com>")]
+[assembly: AssemblyCompany("Jared McGuire")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c585a997-098b-4005-944e-2647869441b7")]
-[assembly: AssemblyVersion("1.0.39.0817")]
-[assembly: AssemblyFileVersion("1.0.39.0817")]
+[assembly: AssemblyVersion("1.0.41.0819")]
+[assembly: AssemblyFileVersion("1.0.41.0819")]
